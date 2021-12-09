97dec9bdd931071827da49c878ef3334fa8025f5
EmmaGoodliffe <emma@piemag.com>
2021-12-09 21:05:29 +0000

    paginated meme archive

--- src/Archive.svelte
+++ src/Archive.svelte
@@ -9,15 +9,39 @@
   const today = new Date();
   const dates = Object.keys(memes.otd);
 
-  const isArchived = (date: string) => {
+  const isArchived = (date: string, month: number, year: number) => {
     const d = new Date(date);
     d.setDate(d.getDate() - 1);
-    const weekBefore = new Date();
-    weekBefore.setDate(today.getDate() - 28);
-    return weekBefore <= new Date(d) && new Date(d) <= today;
+    // const weekBefore = new Date();
+    // weekBefore.setDate(today.getDate() - 28);
+    // return weekBefore <= new Date(d) && new Date(d) <= today;
+    const goodMonth = d.getMonth() + 1 === month;
+    const goodYear = d.getFullYear() === year;
+    return goodMonth && goodYear && new Date(d) <= today;
   };
 
+  let month = today.getMonth() + 1;
+  let year = today.getFullYear();
   let dateString = dateToString(today);
+  let forwardsEnabled = true;
+  let backwardsEnabled = true;
+
+  $: {
+    if (month < 1) {
+      month = 12;
+      year--;
+    } else if (month > 12) {
+      month = 1;
+      year++;
+    }
+  }
+
+  $: forwardsEnabled = !(
+    today.getMonth() + 1 === month && today.getFullYear() === year
+  );
+
+  $: backwardsEnabled = new Date(2021, 9 - 1, 1) < new Date(year, month - 1, 1);
+
   $: matchingDates = dates.filter(
     date =>
       dateToString(new Date(date)) === dateToString(stringToDate(dateString)),
@@ -27,7 +51,29 @@
 <Header />
 <main>
   <h2>Meme archive</h2>
-  <p class="my-2">The last month</p>
+  <div class="flex sm:w-1/4 mx-auto my-4">
+    <div class="w-1/4">
+      <div
+        class="flex-1 btn"
+        on:click={() => backwardsEnabled && month--}
+        disabled={!backwardsEnabled}
+      >
+        <span class="-mt-1.5">&lt;</span>
+      </div>
+    </div>
+    <p class="flex-1 my-2 text-lg text-center">
+      {`${month}`.padStart(2, "0")}/{year}
+    </p>
+    <div class="w-1/4">
+      <div
+        class="flex-1 btn"
+        on:click={() => forwardsEnabled && month++}
+        disabled={!forwardsEnabled}
+      >
+        <span class="-mt-1.5">&gt;</span>
+      </div>
+    </div>
+  </div>
   <table class="table-auto w-full max-w-4xl mx-auto border-white border-2">
     <thead>
       <tr>
@@ -37,7 +83,7 @@
     </thead>
     <tbody>
       {#each dates as date}
-        {#if isArchived(date)}
+        {#if isArchived(date, month, year)}
           <tr>
             <td class="text-center border-2 p-4">
               {date.split("-").reverse().join("/")}
@@ -61,11 +107,7 @@
 </main>
 <section class="mt-48">
   <h2>Specify a date</h2>
-  <input
-    type="text"
-    class="bg-bg border-white border px-2 py-1"
-    bind:value={dateString}
-  />
+  <input type="text" bind:value={dateString} />
   {#if matchingDates.length}
     <img
       class="max-w-sm mx-auto w-1/2 sm:w-auto"
